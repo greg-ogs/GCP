211490ee8957369143d7821169e982b8df01c7ec
Alex <65621681+greg-ogs@users.noreply.github.com>
2025-05-05 18:12:24 -0600

    Update sidecar proxy images, ports, auth, and registry setup
    Updated container image versions and ports in `docker-compose.yml` and `Dockerfile`. Enhanced authentication by integrating Google Cloud secrets and adding Cloud SDK installation in the Docker image. Re-enabled and refined the Docker registry creation script for better image handling.

--- shell_scripts/google-kubernetes/sidecar-proxy/entrypoint.sh
+++ shell_scripts/google-kubernetes/sidecar-proxy/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status.
-set -e
+
+export PROJECT_ID=gc-bootcamp-14dfb3bf
+
+gcloud auth activate-service-account --key-file=/app/mnt/key.json
+
+ gcloud config set project "${PROJECT_ID}"
 
 # Start the Cloud SQL Proxy in the background.
 # Use the INSTANCE_CONNECTION_NAME environment variable.
 # The '&' sends it to the background.
-export DB_USER="${DB_USER}"
-export DB_PASS="${DB_PASS}"
-export DB_NAME="${DB_NAME}"
-export INSTANCE_CONNECTION_NAME="${INSTANCE_CONNECTION_NAME}"
-export GOOGLE_APPLICATION_CREDENTIALS="/app/key.json"
+export DB_USER=$(gcloud secrets versions access latest --secret="DB_USER" --project=$PROJECT_ID)
+export DB_PASS=$(gcloud secrets versions access latest --secret="DB_PASS" --project=$PROJECT_ID)
+export DB_NAME=$(gcloud secrets versions access latest --secret="DB_NAME" --project=$PROJECT_ID)
+export INSTANCE_CONNECTION_NAME=$(gcloud secrets versions access latest --secret="INSTANCE_CONNECTION_NAME" --project=$PROJECT_ID)
+
+echo "$(gcloud secrets versions access latest --secret="key" --project="$(gcloud config get-value project)")" > /app/key.json
+export GOOGLE_APPLICATION_CREDENTIALS="/app/mnt/key.json"
 
 # Activate the virtual environment
 source /app/main/bin/activate
