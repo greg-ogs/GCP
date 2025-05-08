b3816f2a35ad2b2580fe79af29ddeeeb16f78d37
Alex <65621681+greg-ogs@users.noreply.github.com>
2025-05-08 15:57:49 -0600

    Add new outputs for IDs and metadata in Terraform config
    These changes add outputs for VPC ID, subnetwork IDs, bucket ID, and project metadata ID. This enhances the configuration's flexibility by exposing these values for use in other modules or external integrations.

--- terraform/base/outputs.tf
+++ terraform/base/outputs.tf
@@ -3,6 +3,10 @@ output "vpc_name" {
   value = google_compute_network.vpc_network.name
 }
 
+output "vpc_id" {
+  value = google_compute_network.vpc_network.id
+}
+
 output "subnetwork_central_name" {
   value = google_compute_subnetwork.subnetwork_central.name
 }
@@ -11,9 +15,25 @@ output "subnetwork_east_name" {
   value = google_compute_subnetwork.subnetwork_east.name
 }
 
+output "subnetworks_ids" {
+  value = toset([
+    google_compute_subnetwork.subnetwork_central.id,
+    google_compute_subnetwork.subnetwork_east.id
+  ])
+}
+
 # Output the bucket name
 output "bucket_name" {
   value = google_storage_bucket.epam_lab_bucket.name
+}
+
+output "bucket_id" {
+  value = google_storage_bucket.epam_lab_bucket.id
+}
+
+# Output the project metadata id
+output "project_metadata_id" {
+  value = google_compute_project_metadata.ssh_keys.id
 }
 
 # Output the service account email for reference
