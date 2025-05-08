3929a0339fee2c9397eb83ab7e41ccb6a491898c
Alex <65621681+greg-ogs@users.noreply.github.com>
2025-05-08 17:41:22 -0600

    Refactor Terraform structure and define GCP compute resources
    Moved compute-related configurations to a dedicated directory. Added remote state data source to reference base outputs and defined GCP provider, variables, and necessary configurations for compute resources.

---
+++ terraform/compute/main.tf
@@ -0,0 +1,18 @@
+# To run use docker run --rm -it -v ./terraform/base:/terraform gregogs/terraform:base apply \
+# -var="project_id=$(gcloud config get-value project)" -var="ssh_key=greg-ogs:$(cat /home/greg-ogs/.ssh/id_rsa.pub)"
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  credentials = file("/terraform/.gcp/credentials.json")
+  region      = "us-central1"
+}
+
